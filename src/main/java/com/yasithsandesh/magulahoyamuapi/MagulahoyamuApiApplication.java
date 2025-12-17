e18fc0066a6f1f44b0f591a11f30f75d616e594e
Yasith Sandesh <yasithsandeshvithanage2002@gmail.com>
2025-12-17 20:41:28 +0530

    fixed

--- src/main/java/com/yasithsandesh/magulahoyamuapi/MagulahoyamuApiApplication.java
+++ src/main/java/com/yasithsandesh/magulahoyamuapi/MagulahoyamuApiApplication.java
@@ -4,16 +4,21 @@ import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.File;
+
 @SpringBootApplication
 public class MagulahoyamuApiApplication {
 
     public static void main(String[] args) {
 
-        Dotenv dotenv = Dotenv.load();
-        dotenv.entries().forEach(e ->
-                System.setProperty(e.getKey(), e.getValue())
-        );
-
+        // Only load .env if exists
+        File envFile = new File(".env");
+        if(envFile.exists()) {
+            Dotenv dotenv = Dotenv.load();
+            dotenv.entries().forEach(e ->
+                    System.setProperty(e.getKey(), e.getValue())
+            );
+        }
         SpringApplication.run(MagulahoyamuApiApplication.class, args);
 
 
